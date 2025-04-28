699adfb2f228fea67ef8b520e3e98aeb843d6812
dream <z2068946849@163.com>
2025-04-28 14:54:41 +0800

    feat:update

--- blog/_site/assets/js/data/swconf.js
+++ blog/_site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1745748287',resources: [
+    cacheName: 'chirpy-1745823227',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
