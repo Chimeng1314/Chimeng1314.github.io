0f6198d1231a979df1c64bd5fa8c5c2f9063099a
dream <z2068946849@163.com>
2025-05-16 16:42:02 +0800

    feat: update VitePress config with new title, description, and theme

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,13 +2,39 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "My Awesome Project",
-  description: "A VitePress Site",
+  // 站点的大标题
+  title: "咸鱼的终极梦想之地",
+  // 站点的描述信息
+  description: "相当一条咸鱼吗？想要获得力量吗？那就快来加入我们吧！",
+  // 站点的导航栏配置信息
   themeConfig: {
+
+    // 站点 logo
+    logo: './static/logo/星星.svg',
     // https://vitepress.dev/reference/default-theme-config
+
+    // 是否显示当前的站点标题 false 隐藏 不写默认展示
+    // siteTitle: false,
+
+    // 导航栏跳转
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      {
+        text: 'Python',
+        items: [
+          { text: 'Item A', link: '/item-1' },
+          { text: 'Item B', link: '/item-2' },
+          { text: 'Item C', link: '/item-3' }
+        ]
+      },
+      {
+        text: 'Go',
+        items: [
+          { text: 'Item A', link: '/item-1' },
+          { text: 'Item B', link: '/item-2' },
+          { text: 'Item C', link: '/item-3' }
+        ]
+      }
+
     ],
 
     sidebar: [
@@ -24,5 +50,12 @@ export default defineConfig({
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
     ]
+  },
+
+  // 页脚配置信息
+  footer: {
+    message: 'Released under the <a href="https://github.com/vuejs/vitepress/blob/main/LICENSE">MIT License</a>.',
+    copyright: 'Copyright © 2019-present <a href="https://github.com/yyx990803">Evan You</a>'
   }
+
 })
